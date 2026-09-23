b6748a2b3130725208852ad5990f6394b68ff13d
kaka <kexin.cola@outlook.com>
2026-09-23 14:45:38 +0000

    thther 0.1.3

--- Formula/thther.rb
+++ Formula/thther.rb
@@ -1,26 +1,26 @@
 class Thther < Formula
   desc "TCP-based mosh-like persistent remote terminal with SSH bootstrap"
   homepage "https://github.com/lakakala/thther-tty"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     depends_on arch: :arm64
 
     on_arm do
-      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.2/thther-v0.1.2-aarch64-apple-darwin.tar.gz"
-      sha256 "470c614718a3631b87c716b9548c5f1784f1e72a0d521b9be3b1ca1b6288db65"
+      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.3/thther-v0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "edc96d5e7e4d1354ab0da28c4ba6adbb0e8a77fc26ca07b54064eea12d6350d4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.2/thther-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ee6c055f6601bb30089125d421abfdd536691a1af970fa3b7ac116670f0ae083"
+      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.3/thther-v0.1.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d7a638a5335942f091dc6ace35bfbd72799411b29931af3f9227d5d93ffbb5fa"
     end
     on_intel do
-      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.2/thther-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5ce50a7709c72c1ba9fb27fb665cdbc1b6161e135d2f5d922c8036b77df9dbce"
+      url "https://github.com/lakakala/thther-tty/releases/download/v0.1.3/thther-v0.1.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f5edf39f2b860351dd888bd692807203639ed228699660b8c0ba2b5089f7a242"
     end
   end
 
